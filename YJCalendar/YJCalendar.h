deef95985dc78ad1b388e9be6f858d37b13a501e
YoonJu-ho <yoonju-ho@yoonjuhouimini.mshome.net>
2015-12-01 23:28:44 +0900

    Initial Commit

---
+++ YJCalendar/YJCalendar.h
@@ -0,0 +1,19 @@
+//
+//  YJCalendar.h
+//  YJCalendar
+//
+//  Created by YoonJu-ho on 2015. 12. 1..
+//  Copyright © 2015년 Yoon Ju-ho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for YJCalendar.
+FOUNDATION_EXPORT double YJCalendarVersionNumber;
+
+//! Project version string for YJCalendar.
+FOUNDATION_EXPORT const unsigned char YJCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YJCalendar/PublicHeader.h>
+
+
